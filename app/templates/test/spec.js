6eb2297e9b3991bb517f2a6ffd5c1c9743bceead
metaraine <raineorshine@gmail.com>
2016-01-04 15:28:07 -0700

    Describe test in spec.

--- app/templates/test/spec.js
+++ app/templates/test/spec.js
@@ -2,7 +2,7 @@ var chai = require('chai')
 var should = chai.should()
 var <%=camelize(project)%> = require('../index.js')
 
-describe('doSomething', function () {
+describe('<%=camelize(project)%>', function () {
   it('should do something', function () {
     <%=camelize(project)%>.doSomething().should.equal(12345)
   })
