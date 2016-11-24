be7ce41e1dedbab2aed0618dcdf6c157d88cb46a
Raine Revere <raineorshine@gmail.com>
2016-11-23 17:20:57 -0700

    Do not camelCase the spec describe block title.

--- app/templates/test/spec.js
+++ app/templates/test/spec.js
@@ -2,7 +2,7 @@ import * as chai from 'chai'
 import <%=camelize(project)%> from '../<%=babel ? 'src/' : ''%>index.js'
 const should = chai.should()
 
-describe('<%=camelize(project)%>', () => {
+describe('<%=project%>', () => {
   it('should do something', () => {
     <%=camelize(project)%>.doSomething().should.equal(12345)
   })
