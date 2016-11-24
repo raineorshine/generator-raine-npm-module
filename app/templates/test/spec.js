4c645fb68b1a5d9a5d16def8455ed8268cb25386
Raine Revere <raineorshine@gmail.com>
2016-11-23 17:27:18 -0700

    Only use es6 imports if babel is chosen.

--- app/templates/test/spec.js
+++ app/templates/test/spec.js
@@ -1,9 +1,14 @@
+>> if(babel) {
 import * as chai from 'chai'
 import <%=camelize(project)%> from '../<%=babel ? 'src/' : ''%>index.js'
+>> } else {
+const chai = require('chai')
+const <%=camelize(project)%> = require('../<%=babel ? 'src/' : ''%>index.js')
+>> }
 const should = chai.should()
 
 describe('<%=project%>', () => {
   it('should do something', () => {
-    <%=camelize(project)%>.doSomething().should.equal(12345)
+    <%=camelize(project)%>().should.equal(12345)
   })
 })
